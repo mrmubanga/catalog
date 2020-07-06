a18ce7b64c07e54c4875e90ee084c8bfc8fcb588
Emmanuel_Mubanga <emmanuelmubangaa@gmail.com>
2020-07-06 11:43:42 +0200

    Update recent.php

--- includes/client/recent.php
+++ includes/client/recent.php
@@ -27,20 +27,33 @@ if (!count($records)) {
 } else {
 ?>
 			<?php foreach ($records as $r) { ?>
-				<div class="row" id="prc">
-				<div class="col-xs-6 col-md-3">
-				<a href="index.php?<?php echo escape($r->storeName);?>" class="thumbnail">
-				<?php echo '<img class=\'img-responsive\' width=\' 190px\' height=\' 190px\'src="'.escape($r->image).'"alt="'.escape($r->prodName).'"\'s Profile Image>';?>
-				</a>
-				</div>
-					<p><?php echo '<img class=\'img-circle\' width=\' 50px\' height=\' 50px\'src="'.escape($r->logo).'"alt="'.escape($r->storeName).'"\'s Profile Image>';?>
-					<strong>Store Name<i class="glyphicon glyphicon-home"></i><i class="glyphicon glyphicon-option-vertical"></i></strong><?php echo escape($r->storeName);?>
-					<strong><i class="glyphicon glyphicon-menu-hamburger"></i> HQ Address <i class="glyphicon glyphicon-map-marker"></i><i class="glyphicon glyphicon-option-vertical"></i></strong><?php echo "  ".escape($r->address);?>
-					<strong><i class="glyphicon glyphicon-menu-hamburger"></i> Contact No. <i class="glyphicon glyphicon-earphone"></i><i class="glyphicon glyphicon-option-vertical"></i></strong><?php echo escape($r->contact);?></p>
-					<p><strong>Product Name <i class="glyphicon glyphicon-shopping-cart"></i><i class="glyphicon glyphicon-option-vertical"></i></strong><?php echo escape($r->prodName);?>
-					<strong><i class="glyphicon glyphicon-menu-hamburger"></i> Price <i class="glyphicon glyphicon-tag"></i><i class="glyphicon glyphicon-option-vertical"></i></strong> <?php echo "K".escape($r->price);?></p>
-					<p><a href="index.php?<?php  echo escape($r->prodName);?>"><button class="btn btn-info">Compair <?php echo escape($r->prodName);?> Prices</button></a></p>
-				</div>
+				<div class="row">
+				<div class="col-sm-4">
+    
+    <!--Card image-->
+    <div class="view overlay panel panel-default">
+	<?php echo '<img class=\'img-responsive\' width=\' 100%\' height=\' 200px\'src="'.escape($r->image).'"alt="'.escape($r->prodName).'"\'s Profile Image>';?>      
+	<a href="#!">
+      <a href="#!">
+        <div class="mask rgba-white-slight"></div>
+      </a>
+    </div>
+    <!--Card content-->
+    <div class="card-body">
+      <!--Title-->
+					<br>
+					<strong>Store Name <i class="glyphicon glyphicon-home"></i><i class="glyphicon glyphicon-option-vertical"></i></strong><?php echo escape($r->storeName);?><br>
+					<strong> HQ Address    <i class="glyphicon glyphicon-map-marker"></i><i class="glyphicon glyphicon-option-vertical"></i></strong><?php echo "  ".escape($r->address);?><br>
+					<strong> Contact No.   <i class="glyphicon glyphicon-earphone"></i><i class="glyphicon glyphicon-option-vertical"></i></strong><?php echo escape($r->contact);?></p>
+					<p><strong>Product Name <i class="glyphicon glyphicon-shopping-cart"></i><i class="glyphicon glyphicon-option-vertical"></i></strong><?php echo escape($r->prodName);?><br>
+					<strong>Price <i class="glyphicon glyphicon-tag"></i><i class="glyphicon glyphicon-option-vertical"></i></strong> <?php echo "K".escape($r->price);?></p>
+					<!-- <p><a href="index.php?<?php  echo escape($r->prodName);?>"><button class="btn btn-info">Compair <?php echo escape($r->prodName);?> Prices</button></a></p> -->
+					<p><a href="https://www.google.co.zm/maps/search/nearest+<?php echo escape($r->storeName);?>+store/@-15.4137265,28.2851624,13z/data=!3m1!4b1"  target="blank"><button class="btn btn-info">View <?php echo escape($r->storeName);?> Stores Near You</button></button></a></p>
+
+    </div>
+
+  </div>
+  </div>
 				<hr/>
 			<?php } 
 }/*<del><?php echo escape($r->price);?></del>*/
